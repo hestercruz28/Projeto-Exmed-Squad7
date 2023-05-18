6c844f3c193dc60566b644645972f21668cfd95a
LucasSilva1207 <132783522+LucasSilva1207@users.noreply.github.com>
2023-05-18 18:37:41 -0300

    Delete Projeto-Exmed-Squad7/riseup directory

--- Projeto-Exmed-Squad7/riseup/Tela de Cadastro/js/componentes/validarFormatos/validarFormatoSenha.js
+++
@@ -1,7 +0,0 @@
-function validarFormatoSenha(senha) {
-  // A senha deve ter pelo menos 8 caracteres, incluindo pelo menos um dígito, uma letra maiúscula e uma minúscula
-  const regexSenha = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
-  return regexSenha.test(senha);
-}
-
-export default validarFormatoSenha
