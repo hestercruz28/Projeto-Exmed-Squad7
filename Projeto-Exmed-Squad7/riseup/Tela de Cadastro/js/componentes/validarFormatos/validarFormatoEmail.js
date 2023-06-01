44dd4db50bf28bcf9d17f3832f9f0dea0d8ea1c0
LucasSilva1207 <132783522+LucasSilva1207@users.noreply.github.com>
2023-06-01 18:27:10 -0300

    Delete Projeto-Exmed-Squad7/riseup directory

--- Projeto-Exmed-Squad7/riseup/Tela de Cadastro/js/componentes/validarFormatos/validarFormatoEmail.js
+++
@@ -1,6 +0,0 @@
-function validarFormatoEmail(txtEmail) {
-    let re = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/gi
-    return re.test(txtEmail)
-}
-
-export default validarFormatoEmail
--- Projeto-Exmed-Squad7/riseup/Tela de Cadastro/js/componentes/validarFormatos/validarFormatoNome.js
+++
@@ -1,6 +0,0 @@
-function validarFormatoNome(txtNome) {
-    let re = /^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+$/
-    return re.test(txtNome)
-}
-
-export default validarFormatoNome
